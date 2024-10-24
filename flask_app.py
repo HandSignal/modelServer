7e1aa6f9398320aa8b94de0d7f5da69a53ad26c2
JoJosmin <ead3070@naver.com>
2024-10-24 20:25:29 +0900

    주석정리

--- flask_app.py
+++ flask_app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://43.203.16.219:8080"}})
 
 model_path = "/home/ubuntu/modelServer/src/train_All_test_embedding_state.pth"
-#model = torch.load(model_path, map_location=torch.device('cpu'))
+
 # 모델 객체 생성
-pose_input_dim = 4  # 적절한 입력 차원 설정
+pose_input_dim = 4  
 hand_input_dim = 6
 meaning_input_dim = 768
 hidden_dim = 512
@@ -21,10 +21,6 @@ output_dim = 768
 model = SignLanguageTranslationModel(pose_input_dim, hand_input_dim, hidden_dim, output_dim)
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 model.eval()  
-
-# meaning_dict_path = "/home/choijjyoGrad/modelServer/src/meaning_dict.pkl"
-# with open(meaning_dict_path, 'rb') as f:
-#   meaning_dict = pickle.load(f)
 
 embedding_dict_path = "/home/ubuntu/modelServer/src/embedding_dict_new.pkl"
 with open(embedding_dict_path, 'rb') as f:
@@ -40,9 +36,9 @@ def preprocess_keypoints(keypoints, input_dim, target_num_keypoints):
 
     if input_dim == 4:
         visibility = [kp.get('visibility', 1.0) for kp in keypoints]
-        tensor = torch.tensor([x, y, z, visibility]).float().unsqueeze(0)  # (1, 4, num_keypoints)
+        tensor = torch.tensor([x, y, z, visibility]).float().unsqueeze(0)  
     else:
-        tensor = torch.tensor([x, y, z]).float().unsqueeze(0)  # (1, 3, num_keypoints)
+        tensor = torch.tensor([x, y, z]).float().unsqueeze(0)  
     # 패딩 적용
     num_keypoints = tensor.size(2)
     if num_keypoints < target_num_keypoints:
@@ -51,8 +47,9 @@ def preprocess_keypoints(keypoints, input_dim, target_num_keypoints):
     elif num_keypoints > target_num_keypoints:
         tensor = tensor[:, :, :target_num_keypoints]  # 넘치는 부분 잘라냄
 
-    return tensor.transpose(1, 2)  # (1, target_num_keypoints, input_dim)
+    return tensor.transpose(1, 2) 
 
+#입력값 통한 의미 추론
 def infer_meaning(model, pose_keypoints, left_hand_keypoints, right_hand_keypoints, embedding_dict):
     max_num_keypoints_pose = 33  # 포즈의 최대 키포인트 수
     max_num_keypoints_hand = 21  # 손의 최대 키포인트 수
@@ -72,11 +69,10 @@ def infer_meaning(model, pose_keypoints, left_hand_keypoints, right_hand_keypoin
     with torch.no_grad():
         outputs = model(pose_tensor, torch.cat((left_hand_tensor, right_hand_tensor), dim=2))
 
-    # 임베딩 결과와 비교하기 위해 모델의 출력 값을 얻음
     model_embedding = outputs.squeeze(0)  # 모델 출력의 임베딩 벡터화
     
     # 코사인 유사도를 사용하여 가장 유사한 임베딩을 찾음
-    best_similarity = -1  # 초기값은 매우 낮은 유사도
+    best_similarity = -1  
     predicted_meaning = None
 
     # embedding_dict의 각 임베딩과 모델 출력 임베딩 간의 코사인 유사도 계산
@@ -100,11 +96,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
+
+#전달 받은 S3 URL을 통해 동작의 좌표를 추출한 json 파일 받아옴
 def predict():
     data = request.get_json()
     json_url = data['s3url'].strip('\"')
     
-    #json_url = r"https://hand-coordinates-json.s3.ap-northeast-2.amazonaws.com/%EB%8B%B5_%EA%B3%A0%EB%AF%BC.json"
     try:
         json_data = load_json_from_url(json_url)
     except Exception as e:
